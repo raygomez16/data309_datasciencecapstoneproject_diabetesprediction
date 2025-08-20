7f0639e817aa4c4270c83f9961bdbd1e0bbe403c
immortalBz <jmcm9947@gmail.com>
2025-08-20 12:58:38 +1200

    Updated dataset with bmi ranges

--- 01_preprocessing_jma538.R
+++ 01_preprocessing_jma538.R
@@ -2,14 +2,14 @@
 library('tidyverse')
 library('dplyr')
 library('ggplot2')
+library('here')
 
-data <- 'https://raw.githubusercontent.com/arinbasu/data309/refs/heads/main/diabetes_prediction_data.csv'
-diab_dataset <- read.csv(data)
+#env <- new.env(parent = emptyenv())
+#sys.source(here("00_cleaning_final.R"), envir = env)
+
+diab_dataset <- source("00_cleaning_final.R")$value
 head(diab_dataset)
 
-#Getting the assigned variables 
-#jma_dataset <- diab_dataset[,c('smoking_history', 'bmi')]
-#jma_dataset
 
 #Cleaning the data
 clean_dataset <- na.omit(diab_dataset)
@@ -25,7 +25,7 @@ clean_dataset$bmi <- as.numeric(clean_dataset$bmi)
 clean_dataset$smoking_history <- tolower(clean_dataset$smoking_history)
 class(clean_dataset$smoking_history)
 class(clean_dataset$bmi)
-
+ 
 
 ###
 
@@ -33,18 +33,31 @@ class(clean_dataset$bmi)
 #Data summaries
 clean_dataset %>% count(clean_dataset$smoking_history)
 
+#Box plot for BMI
 bmi_ranges <- boxplot(clean_dataset$bmi, main = "Individual's BMI Levels", xlab="BMI Level", horizontal = TRUE)
 summary(clean_dataset$bmi)
 
-smoking_history_graph <- ggplot(clean_dataset, aes(x=smoking_history)) + geom_bar(fill="steelblue") + labs(title = "Smoking History Bar Plot", x="Smoking History", y="Count") + geom_text(stat = "count", aes(label = ..count..), vjust = -0.5)
-smoking_history_graph
+#Filtering out 'other' gender as the population is too small to be considered effective.
+gender_subset <- filter(clean_dataset, clean_dataset$gender %in% c('female','male'))
 
-#bmi plot with gender comparison
-ggplot(clean_dataset, aes(age, bmi)) + geom_point(aes(colour = gender), alpha = 0.5) + facet_wrap(~gender)
+#smoking history count graph.
+smoking_history_graph <- ggplot(gender_subset, aes(x=smoking_history)) + geom_bar(fill="steelblue") + labs(title = "Smoking History Bar Plot", x="Smoking History", y="Count") + geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5)
+smoking_history_graph
 
 #smoking_history plot based on gender
 ggplot(clean_dataset, aes(smoking_history, fill=gender)) + labs(title = "Smoking History based on Gender", x = "Smoking history",
-                                                                y = "Count") + geom_bar(position = "stack") + scale_fill_manual(values = c("Female" = "pink", "Male" = "lightblue"))
+                                                                y = "Count") + geom_bar(position = "stack") + scale_fill_manual(values = c("female" = "pink", "male" = "lightblue"))
+
+#bmi plot with gender comparison
+#ggplot(gender_subset, aes(age, bmi)) + geom_point(aes(colour = gender), alpha = 0.5) + facet_wrap(~gender)
+
+#generating value ranges for BMI data
+custom_intervals <- c(0, 18.5, 24.9, 29.9, 49.9, 100)
+bmi_data <- gender_subset$newcol %>% 
+  cut(gender_subset$bmi, breaks = custom_intervals, include.lowest = TRUE, labels = c("0-18.5", "18.5-24.9", "25-29.9", "30-49.9", "50-100"))
+
+ggplot(bmi_data, aes(age, bmi)) + geom_histogram(aes(colour = gender), alpha = 0.5) + facet_wrap(~gender)
+
 
 
 
